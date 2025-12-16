f2cb5199b986fdc1b6672f32fda55a04ed5807d7
Vear <arnuwat.vear@gmail.com>
2025-12-16 15:55:55 +0700

    Finish lab8_3

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,48 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+    string crew;
+    int age;
+    int height;
+    int bountry;
+
+    cout << "Enter your age: ";
+    cin >> age;
+    if (age <= 25){
+        cout << "Enter your height: ";
+        cin >> height;
+        if (height < 100){
+            crew = "Chopper";
+        }
+        else if (height < 180){
+            crew = "Usopp";
+        }
+        else{
+            cout << "Enter your bounty: ";
+            cin >> bountry;
+            if (bountry > 1.1e9){
+                crew = "Zoro";
+            }
+            else{
+                crew = "Sanji";
+            }
+        }
+    }
+    else if (age <= 60){
+        cout << "Enter your bounty: ";
+        cin >> bountry;
+        if (bountry > 5e8){
+            crew = "Jinbe";
+        }
+        else{
+            crew = "Franky";
+        }
+    }
+    else{
+        crew = "Brook";
+    }
+    cout << "Your character = " << crew;
+}
